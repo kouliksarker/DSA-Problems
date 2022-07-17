ccc4e75532d6bc1470a60fa3bbd57af004d1ea5d
kouliksarker <koulik.sarker@gmail.com>
2022-07-17 14:19:34 +0530

    Remove Element Problem

---
+++ Solved-Problems/Remove-element.js
@@ -0,0 +1,20 @@
+var removeElement = function(nums, val) {
+    
+    let l = 0;
+    let r = 0;
+    
+    while(r < nums.length){
+        
+        if(nums[r] !== val){
+            
+            nums[l] = nums[r];
+            l++;
+        }
+        
+        r++;
+    }
+    
+    return l;
+    
+    
+};
